0a144716bf93fbf7aebaf9f063e4be7e20617713
Gleb Gaiduk <glebgaiduk.pl@gmail.com>
2021-07-15 20:22:46 +0200

    create routers

--- server/index.js
+++ server/index.js
@@ -4,12 +4,14 @@ const express = require('express');
 const sequalize = require('./db');
 const models = require('./models/models');
 const cors = require('cors');
+const router = require('./routes/index');
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
+app.use('/api', router);
 
 const start = async () => {
   try {

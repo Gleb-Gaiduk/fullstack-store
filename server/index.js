9868b7480f20fbc59e3f8b94d981df0935b37b22
Gleb Gaiduk <glebgaiduk.pl@gmail.com>
2021-07-16 20:39:21 +0200

    Add Error middleware

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const sequalize = require('./db');
 const models = require('./models/models');
 const cors = require('cors');
 const router = require('./routes/index');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 
 const app = express();
 const PORT = process.env.PORT;
@@ -12,6 +13,9 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use('/api', router);
+
+// Last middleware
+app.use(errorHandler);
 
 const start = async () => {
   try {
